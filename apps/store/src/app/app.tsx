30b1540922f40ae8125be0b71bbc79e404317072
Daniell Marafon <daniellmarafon@gmail.com>
2022-12-18 04:37:31 +0100

    feat(boil): addin boilerplate for cash flow web project

--- apps/store/src/app/app.tsx
+++ apps/store/src/app/app.tsx
@@ -1,11 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
+import { CommonUi } from '@cashflowweb/common-ui';
 import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
     <>
       <NxWelcome title="store" />
+      <CommonUi></CommonUi>
       <div />
     </>
   );
