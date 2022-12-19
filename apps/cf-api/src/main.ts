b783772d8c5316095128e37309d919441513e221
Daniell Marafon <daniellmarafon@gmail.com>
2022-12-19 03:07:00 +0100

    refactor(boil): add formatter settings and fix indentations

--- apps/cf-api/src/main.ts
+++ apps/cf-api/src/main.ts
@@ -1,14 +1,12 @@
-import { initializeServer } from "./server/initializeServer";
-require("dotenv").config();
+import { initializeServer } from './server/initializeServer';
+require('dotenv').config();
 
 const port = process.env.PORT ?? 3333;
 
 (async () => {
-  try {
-
-    await initializeServer(port);
-  } catch {
-
-    process.exit(1);
-  }
-})()
+    try {
+        await initializeServer(port);
+    } catch {
+        process.exit(1);
+    }
+})();
--- apps/cf-api/src/server/index.ts
+++ apps/cf-api/src/server/index.ts
@@ -1,40 +1,34 @@
-import express = require("express");
-import morgan = require("morgan");
-import cors = require("cors");
-import { pool } from "./queries";
+import express = require('express');
+import morgan = require('morgan');
+import cors = require('cors');
+import { pool } from './queries';
 
 const corsOptions = {
-  origin: [
-    "http://localhost:3300",
-    "http://localhost:3333",
-  ],
+    origin: ['http://localhost:4000', 'http://localhost:3333'],
 };
 
 export const app = express();
 
-app.disable("x-powered-by");
+app.disable('x-powered-by');
 
 app.use(cors(corsOptions));
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
-app.use(express.static("uploads"));
+app.use(express.static('uploads'));
 
 app.use(express.json());
 
-
 const getUsers = (request, response) => {
     pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).json(results.rows)
-    })
-  }
+        if (error) {
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    });
+};
 
-  app.get('/users', getUsers)
-
-
+app.get('/users', getUsers);
 
 // app.use(notFoundError);
 
