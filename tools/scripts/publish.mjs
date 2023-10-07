582941081a07323f29e4186ad21d8f34d4f36df1
Daniell Marafon <daniellmarafon@gmail.com>
2023-10-07 13:10:56 +0200

    fix(cf/config): remove comments

--- tools/scripts/publish.mjs
+++ tools/scripts/publish.mjs
@@ -1,12 +1,3 @@
-/**
- * This is a minimal script to publish your package to "npm".
- * This is meant to be used as-is or customize as you see fit.
- *
- * This script is executed on "dist/path/to/library" as "cwd" by default.
- *
- * You might need to authenticate with NPM before running this script.
- */
-
 import { execSync } from 'child_process'
 import { readFileSync, writeFileSync } from 'fs'
 
@@ -58,4 +49,3 @@ try {
 
 // Execute "npm publish" to publish
 execSync(`npm publish --access public --tag ${tag}`)
-
