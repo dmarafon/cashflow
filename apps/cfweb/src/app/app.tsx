a8f9e0fa502bfe9a3fca2774691f869aa5959a0e
Daniell Marafon <daniellmarafon@gmail.com>
2023-10-07 13:07:32 +0200

    fix(cf/config): change app and nx welcome files

--- apps/cfweb/src/app/app.tsx
+++ apps/cfweb/src/app/app.tsx
@@ -1,14 +1,11 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
 
-import NxWelcome from './nx-welcome';
 
-export function App() {
+export const App = () => {
   return (
     <div>
-      <NxWelcome title="cfweb" />
+      'Building'
     </div>
-  );
+  )
 }
 
 export default App;
