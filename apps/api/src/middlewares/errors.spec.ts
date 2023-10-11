06e78180a594c5425432f87f3c0b484920b37766
Daniell Marafon <daniellmarafon@gmail.com>
2023-10-11 02:03:01 +0200

    fix(cf/api): correct error tests and function

--- apps/api/src/middlewares/errors.spec.ts
+++ apps/api/src/middlewares/errors.spec.ts
@@ -1,9 +1,10 @@
 import { Response } from 'express'
+import { ValidationError } from 'express-validation'
 import { generalError, notFoundError } from './errors'
 
 describe('Given a notFoundError function', () => {
   describe('When its invoked', () => {
-    it('it should call the next function with an error', () => {
+    it('Tit should call the next function with an error', () => {
       const nextFunction = jest.fn()
       const error = new Error()
 
@@ -23,7 +24,23 @@ describe('Given an generalError function', () => {
       } as unknown as Response
 
       const expectedError = 500
-      const error = undefined
+      const error = { cause: 500 }
+
+      generalError(error, null, res, null)
+
+      expect(res.status).toHaveBeenCalledWith(expectedError)
+    })
+  })
+
+  describe('When its invoked with a Validation Error', () => {
+    test('Then it should call the responses status method with a 400', () => {
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      } as unknown as Response
+
+      const expectedError = 400
+      const error = new ValidationError({}, {})
 
       generalError(error, null, res, null)
 
--- apps/api/src/middlewares/errors.ts
+++ apps/api/src/middlewares/errors.ts
@@ -2,6 +2,7 @@ import chalk from 'chalk'
 import { debug } from 'console'
 import { config } from 'dotenv'
 import { NextFunction, Request, Response } from 'express'
+import { ValidationError } from 'express-validation'
 import { customError } from '../utils/customError'
 
 config()
@@ -14,18 +15,13 @@ export const notFoundError = (_req: Request, _res: Response, next: NextFunction)
   next(error)
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const generalError = (
-  error: undefined | Error,
-  _req: Request,
-  res: Response,
-  _next: NextFunction
-) => {
-  debug(chalk.red(error?.message || error?.cause))
-  const message = error?.name ?? 'General Error'
-  const statusCode = typeof error?.cause === 'number' ? error.cause : 500
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any
+export const generalError = (error: any, _req: Request, res: Response, _next: NextFunction) => {
+  debug(chalk.red(error.message || error.cause))
+  const message = error.name ?? 'General Error'
+  const statusCode = typeof error.cause === 'number' ? error.cause : 500
 
-  if (error instanceof Error) {
+  if (error instanceof ValidationError) {
     res.status(400).json({ message: 'Bad Request' })
     debug(chalk.bgRedBright(error.message))
   } else {
